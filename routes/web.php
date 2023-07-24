b6ce8307849779601f262dfbf7ae854daf90b098
AlanDagner <alanquenaya@upeu.edu.pe>
2023-07-24 15:22:48 -0500

    conbinacion

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,34 @@
 <?php
 
+use App\Http\Controllers\ProgramaEstudioController;
+use App\Http\Controllers\TrabajoAplicacionController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+Route::resource('/programaEstudios',ProgramaEstudioController::class);
+Route::resource('/trabajoAplicacion',TrabajoAplicacionController::class);
+// Route::get('/trabajoAplicacion/{taplicacion}', [App\Http\Controllers\TrabajoAplicacionController::class, 'show'])->name('trabajoAplicacion.show');
+<?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProgramaEstudioController;
-use Illuminate\Support\Facades\Route;
+
 
 
 Route::get('/login', function () {
@@ -13,3 +36,31 @@ Route::get('/login', function () {
 });
 
 Route::resource('/programaEstudios',ProgramaEstudioController::class);
+
+Route::get('/', function () {
+    return view('home');
+})->middleware('auth');
+
+Route::get('/register', [RegisterController::class, 'create'])
+        ->middleware('auth')
+        ->name('register.index');
+
+Route::post('/register', [RegisterController::class, 'store'])
+        ->name('register.store');
+
+
+Route::get('/login', [SessionsController::class, 'create'])
+        ->middleware('guest')
+        ->name('login.index');
+
+Route::post('/login', [SessionsController::class, 'store'])
+        ->name('login.store');
+
+Route::get('/logout', [SessionsController::class, 'destroy'])
+        ->middleware('auth')
+        ->name('login.destroy');
+
+
+/*Route::get('/Admin/programaEstudios', [AdminController::class, 'index'])
+        ->middleware('auth.admin')
+        ->name('admin.index');*/
